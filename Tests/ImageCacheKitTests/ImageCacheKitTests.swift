12a97ed002460a2129166f31aa667dd2bfd0330f
ShauliAlgawi <160117907+ShauliAlgawi@users.noreply.github.com>
2025-02-04 21:10:09 +0200

    Initial Commit

---
+++ Tests/ImageCacheKitTests/ImageCacheKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import ImageCacheKit
+
+final class ImageCacheKitTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
